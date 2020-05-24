1caf86cc6d3ae7f8801e871ddb568e2d5fa398cf
jjuriz <jjuriz@gmail.com>
2020-05-24 11:22:17 -0400

    Wired up buttons, changed button function.

--- calc.js
+++ calc.js
@@ -5,20 +5,44 @@ let num1 = 10,
     op = undefined;
 
 const operators = {
-    '+': function(a, b) { return num1 + num2 },
-    '-': function(a, b) { return num1 - num2 },
-    '*': function(a, b) { return num1 * num2 },
-    '/': function(a, b) { return num1 / num2 },
+    '+': function (a, b) { return num1 + num2 },
+    '-': function (a, b) { return num1 - num2 },
+    '*': function (a, b) { return num1 * num2 },
+    '/': function (a, b) { return num1 / num2 },
 };
 
-let oneButton = document.getElementById("one");
-let displayNum = document.getElementById("displayNum");
+let oneButton = document.getElementById("one"),
+    twoButton = document.getElementById("two"),
+    threeButton = document.getElementById("three"),
+    fourButton = document.getElementById("four"),
+    fiveButton = document.getElementById("five"),
+    sixButton = document.getElementById("six"),
+    severnButton = document.getElementById("seven"),
+    eightButton = document.getElementById("eight"),
+    nineButton = document.getElementById("nine"),
+    zeroButton = document.getElementById("zero"),
+    addButton = document.getElementById("add"),
+    subtractButton = document.getElementById("sub"),
+    multiplyButton = document.getElementById("multi"),
+    divideButton = document.getElementById("divide"),
+    equalsButton = document.getElementById("equals"),
 
-oneButton.addEventListener('click', function(){
-    num1 = 1;
-    displayNum.innerText = num1;
-    console.log("hell, is this thing even on?")
-    });
+    displayNum = document.getElementById("displayNum");
+// Set number buttons to display and retain all digits up to max allowed. 
+oneButton.addEventListener('click', function () {
+    displayNum.innerText += oneButton.innerText;
+});
+
+twoButton.addEventListener('click', function () {
+    displayNum.innerText += twoButton.innerText;
+});
+
+threeButton.addEventListener('click', function () {
+    displayNum.innerText += threeButton.innerText;
+});
+
+
+
 
 // op = '-';
 // console.log((operators[op](10, 20)))
